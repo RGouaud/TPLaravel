bbe8c6c00ed436394ad7ec57d8e24d3671c4a0b4
RomainG <rgouaud@iutbayonne.univ-pau.fr>
2023-03-21 11:24:24 +0100

    Début du TP2, importation des packages graphiques

--- TP1/database/migrations/2023_03_03_104028_create.table_reservation_vehicule.php
+++ TP1/database/migrations/2023_03_03_104028_create.table_reservation_vehicule.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatetableReservationVehicule extends Migration
+class CreateTableReservationVehicules extends Migration
 {
     /**
      * Run the migrations.
@@ -13,9 +13,8 @@ class CreatetableReservationVehicule extends Migration
      */
     public function up()
     {
-        Schema::create('table_reservation_vehicule', function(bluePrint $table){
+        Schema::create('table_reservation_vehicules', function(bluePrint $table){
             $table -> String('reservationId');
-            $table-> onDelete('cascade');
             $table -> String('vehiculeId');
             $table -> primary('vehiculeId', 'reservationId');      
         });
